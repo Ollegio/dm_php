f29c74be4c1c1298ee1d8224f42678c5101b386d
Ollegio <skfounder1@gmail.com>
2017-05-13 00:03:37 +0400

    Update lab_dm_4.php

--- lab_dm_4.php
+++ lab_dm_4.php
@@ -6,7 +6,7 @@
 <body>
 <form method="post" action="">
     <textarea rows="10" cols="45" name="arr"></textarea>
-    <input type="submit" name="button" value="Îòïðàâèòü">
+    <input type="submit" name="button" value="Ioi?aaeou">
 </form>
 </body>
 </html>
@@ -14,27 +14,63 @@
 function floyd ($matx, $a, $b) {
 	$mat_path = $matx;
 	$mat_vertex = array();
-	foreach($mat_path as $row)
-		foreach($row as $elem)
-			if ($elem == 0)
-				$elem = 100000000;
-	for ($i = 0; i<count($mat_path); i++)
-		for ($j = 0; i<count($mat_path); i++)
-			for ($k = 0; i<count($mat_path); i++)
-				if ($mat_path[$j][$k] > $math_path[$j][$i] + $math_path[$i][$k]) {
-					$math_path[$j][$k] = $math_path[$j][$i] + $math_path[$i][$k];
-					$math_vertex[$j][$k] = $i;
-				}			
-}
+	$inf = 100000000;
+	for ($i = 0; $i < count($mat_path); $i++)
+		for ($j = 0; $j < count($mat_path); $j++) {
+			if ($mat_path[$i][$j] == 0)
+				$mat_path[$i][$j] = $inf;
+			if ($mat_path[$i][$j] == $inf)
+				$mat_vertex[$i][$j] = 0;
+			else
+				$mat_vertex[$i][$j] = $j;
+	}
+	for ($i = 0; $i < count($mat_vertex); $i++) {
+		for ($j = 0; $j < count($mat_vertex); $j++)
+			echo $mat_vertex[$i][$j]." ";
+	echo "</br>";
+	}
+	echo "</br>";
+	for ($i = 0; $i < count($mat_path); $i++)
+		for ($j = 0; $j < count($mat_path); $j++)
+			for ($k = 0; $k < count($mat_path); $k++)
+				if ($mat_path[$j][$k] > $mat_path[$j][$i] + $mat_path[$i][$k]) {
+					$mat_path[$j][$k] = $mat_path[$j][$i] + $mat_path[$i][$k];
+					$mat_vertex[$j][$k] = $mat_vertex[$i][$k];
+				}
+				
+   
+	echo "</br>";
+	for ($i = 0; $i < count($mat_path); $i++) {
+		for ($j = 0; $j < count($mat_path); $j++)
+			echo $mat_path[$i][$j]." ";
+	echo "</br>";
+	}
+	echo "</br>";
+	for ($i = 0; $i < count($mat_vertex); $i++) {
+		for ($j = 0; $j < count($mat_vertex); $j++)
+			echo $mat_vertex[$i][$j]." ";
+	echo "</br>";
+	}
+	$f = $mat_vertex[$a][$b];
+	while (1) {
+	echo " ".$f;
+	$f -= 1;
+	$f = $mat_vertex[$a][$f];
+	if ($f == $a) break;
+	}
 
+}
 if(isset($_POST['button'])) {
     $arr_full = $_POST['arr'];
 	$arr = explode("\n", $arr_full);
 	foreach($arr as $row)
 		$mat[] = explode(" ", $row);
-	 
 	
 	
-	print_r($mat);
+	
+	
+	
+	echo "</br>";
+	floyd($mat, 0, 2);
 }
 ?>
