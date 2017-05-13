1dea25c0b7a00ca2d200e5820273791c4dccdf15
Ollegio <skfounder1@gmail.com>
2017-05-13 16:19:37 +0800

    Update lab_dm_4.php

--- lab_dm_4.php
+++ lab_dm_4.php
@@ -15,8 +15,8 @@ function floyd ($matx, $a, $b) {
 	$mat_path = $matx;
 	$mat_vertex = array();
 	$inf = 100000000;
-	for ($i = 0; $i < count($mat_path); $i++)
-		for ($j = 0; $j < count($mat_path); $j++) {
+	for ($i = 1; $i < count($mat_path) + 1; $i++)
+		for ($j = 1; $j < count($mat_path) + 1; $j++) {
 			if ($mat_path[$i][$j] == 0)
 				$mat_path[$i][$j] = $inf;
 			if ($mat_path[$i][$j] == $inf)
@@ -24,33 +24,40 @@ function floyd ($matx, $a, $b) {
 			else
 				$mat_vertex[$i][$j] = $j;
 	}
-	for ($i = 0; $i < count($mat_vertex); $i++) {
-		for ($j = 0; $j < count($mat_vertex); $j++)
+	echo "//<u>1,2,3,4</u></br>";
+	for ($i = 1; $i < count($mat_vertex) + 1; $i++) {
+		echo "$i|";
+		for ($j = 1; $j < count($mat_vertex) + 1; $j++)
 			echo $mat_vertex[$i][$j]." ";
 	echo "</br>";
 	}
+	
 	echo "</br>";
-	for ($i = 0; $i < count($mat_path); $i++)
-		for ($j = 0; $j < count($mat_path); $j++)
-			for ($k = 0; $k < count($mat_path); $k++)
+	for ($i = 1; $i < count($mat_path) + 1; $i++)
+		for ($j = 1; $j < count($mat_path) + 1; $j++)
+			for ($k = 1; $k < count($mat_path) + 1; $k++)
 				if ($mat_path[$j][$k] > $mat_path[$j][$i] + $mat_path[$i][$k]) {
 					$mat_path[$j][$k] = $mat_path[$j][$i] + $mat_path[$i][$k];
-					$mat_vertex[$j][$k] = $mat_vertex[$i][$k];
+					$mat_vertex[$j][$k] = $mat_vertex[$j][$i];
 				}
 				
    
 	echo "</br>";
-	for ($i = 0; $i < count($mat_path); $i++) {
-		for ($j = 0; $j < count($mat_path); $j++)
+	echo "//<u>1,2,3,4</u></br>";
+	for ($i = 1; $i < count($mat_path) + 1; $i++) {
+		echo "$i|";
+		for ($j = 1; $j < count($mat_path) + 1; $j++)
 			echo $mat_path[$i][$j]." ";
-	echo "</br>";
+		echo "</br>";
 	}
+	
 	echo "</br>";
-	for ($i = 0; $i < count($mat_vertex); $i++) {
-		for ($j = 0; $j < count($mat_vertex); $j++)
+	for ($i = 1; $i < count($mat_vertex) + 1; $i++) {
+		for ($j = 1; $j < count($mat_vertex) + 1; $j++)
 			echo $mat_vertex[$i][$j]." ";
-	echo "</br>";
+		echo "</br>";
 	}
+	
 	$f = $mat_vertex[$a][$b];
 	while (1) {
 	echo " ".$f;
@@ -58,19 +65,20 @@ function floyd ($matx, $a, $b) {
 	$f = $mat_vertex[$a][$f];
 	if ($f == $a) break;
 	}
-
 }
 if(isset($_POST['button'])) {
     $arr_full = $_POST['arr'];
 	$arr = explode("\n", $arr_full);
+
 	foreach($arr as $row)
 		$mat[] = explode(" ", $row);
 	
-	
-	
+	for ($i = 0; $i < count($mat); $i++)
+		for ($j = 0; $j < count($mat); $j++)
+			$mat2[$i + 1][$j + 1] = $mat[$i][$j];
 	
 	
 	echo "</br>";
-	floyd($mat, 0, 2);
+	floyd($mat2, 1, 2);
 }
 ?>
